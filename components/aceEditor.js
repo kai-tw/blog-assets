6692fe626fedf84f5f72a7310cbc9b7b9044e2df
Kai <wuvincentck505@gmail.com>
2023-12-23 02:53:24 +0800

    Add section style in article

---
+++ components/aceEditor.js
@@ -0,0 +1,27 @@
+"use strict";
+(function () {
+    const scriptPromise = new Promise((resolve, reject) => {
+        // Request the script file.
+        const scriptNode = document.createElement("script");
+        scriptNode.onload = () => {
+            resolve();
+        }
+        scriptNode.src = "https://cdn.jsdelivr.net/npm/ace-builds@1.32.0/src-min-noconflict/ace.min.js";
+        document.head.appendChild(scriptNode);
+    });
+    scriptPromise.then(() => {
+        // Request the CSS style file.
+        return new Promise((resolve, reject) => {
+            const linkNode = document.createElement("link");
+            linkNode.onload = () => {
+                resolve();
+            }
+            linkNode.rel = "stylesheet";
+            linkNode.href = "https://cdn.jsdelivr.net/npm/ace-builds@1.32.0/css/ace.min.css";
+        });
+    })
+        .then(() => {
+            // Successfully load all the resources, and initialize.
+
+        });
+})()
