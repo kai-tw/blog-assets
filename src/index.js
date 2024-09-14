f7afb0d1da0c57779a18cfd17193022f523c32bc
Kai <wuvincentck505@gmail.com>
2024-09-15 02:01:24 +0800

    Rename folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "normalize.css";
-import "@src/scss/style.sass";
+import "@src/sass/style.sass";
 import { AceEditor } from "@src/AceEditor.js";
 import { siteNavFunction } from "@src/siteNavFunction.js";
 import { HoyoCode } from "@src/post/HoyoCode.js";
