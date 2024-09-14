633957e97d8c0f73964ac107fa314c24ba3dac50
Kai <wuvincentck505@gmail.com>
2024-09-15 01:59:57 +0800

    SCSS -> SASS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "normalize.css";
-import "@src/scss/style.scss";
+import "@src/scss/style.sass";
 import { AceEditor } from "@src/AceEditor.js";
 import { siteNavFunction } from "@src/siteNavFunction.js";
 import { HoyoCode } from "@src/post/HoyoCode.js";
